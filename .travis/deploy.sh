f285129ccf12363579c0a4ac483c0464c7562f37
Yannick Mayeur <yannick.mayeur@gmail.com>
2019-11-14 17:55:45 +0100

    Update deploy script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -8,4 +8,4 @@ git add ./src/api/Polyteach-218b86e392c9.json
 git commit -m "Add gcs key"
 git remote add deploy dokku@igpolytech.fr:polyteach-video
 git config --global push.default simple
-git push deploy master --force
+git push deploy HEAD:master --force
