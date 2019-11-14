69e409a4175ec98644a377ee22fb3d26174bf815
Yannick Mayeur <yannick.mayeur@gmail.com>
2019-11-14 17:45:17 +0100

    Update deploy script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,6 +4,8 @@ eval "$(ssh-agent -s)"
 chmod 600 .travis/travis
 ssh-add .travis/travis
 ssh-keyscan igpolytech.fr >> ~/.ssh/known_hosts
+git add ./src/api/Polyteach-218b86e392c9.json
+git commit -m "Add gcs key"
 git remote add deploy dokku@igpolytech.fr:polyteach-video
 git config --global push.default simple
 git push deploy master --force
